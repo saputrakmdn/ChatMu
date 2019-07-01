495d7f905146ea58d27529f28bfa0ba11f879bab
saputrakmdn <saputra.kmdn@gmail.com>
2019-07-01 16:33:46 +0700

    message ok with view photo

--- app/src/main/java/latihan/android/com/chatmu/login.kt
+++ app/src/main/java/latihan/android/com/chatmu/login.kt
@@ -11,7 +11,9 @@ import android.R.attr.button
 import android.view.View
 import kotlinx.android.synthetic.main.login.*
 import android.R.string.no
+import android.os.Handler
 import android.util.Log
+import android.util.Log.e
 import com.facebook.*
 import com.facebook.login.widget.LoginButton
 import com.google.android.gms.common.SignInButton
@@ -20,6 +22,18 @@ import com.google.firebase.auth.FirebaseAuth
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.LoginResult
 import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.database.*
+import com.google.firebase.internal.FirebaseAppHelper.getUid
+
+import com.google.firebase.auth.UserInfo
+import com.google.firebase.auth.FirebaseUser
+import latihan.android.com.chatmu.activity.ChatDetailsActivity
+import latihan.android.com.chatmu.activity.MainActivity
+import latihan.android.com.chatmu.data.SettingApi
+import latihan.android.com.chatmu.utilities.Const
+import latihan.android.com.chatmu.utilities.Const.Companion.NODE_ID
+import latihan.android.com.chatmu.utilities.Const.Companion.NODE_NAME
+import latihan.android.com.chatmu.utilities.Const.Companion.NODE_PHOTO
 
 
 class login: AppCompatActivity() {
@@ -33,17 +47,24 @@ class login: AppCompatActivity() {
     var mobile: EditText? = null
     var button: Button? = null
     var no: String? = null
+    internal lateinit var set: SettingApi
+     lateinit var ref: DatabaseReference
+    val USERS_CHILD = "users"
+    var facebookUserId = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FacebookSdk.sdkInitialize(applicationContext)
         AppEventsLogger.activateApp(this)
         setContentView(R.layout.login)
-        mobile = findViewById(R.id.et_login)
+        set = SettingApi(this)
+
         facebookBtn = findViewById<View>(R.id.fb_btn) as LoginButton
         firebaseAuth = FirebaseAuth.getInstance()
         callbackManager = CallbackManager.Factory.create()
-        facebookBtn.setReadPermissions("email")
+        ref = FirebaseDatabase.getInstance().getReference(USERS_CHILD)
+        facebookBtn.setReadPermissions("public_profile", "email")
+
         facebookBtn.registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
             override fun onSuccess(result: LoginResult) {
                 handleFacebookAccessToken(result.accessToken)
@@ -58,17 +79,11 @@ class login: AppCompatActivity() {
             }
 
         })
-        btn_login.setOnClickListener(View.OnClickListener {
-                no = mobile?.getText().toString()
-                validNo(no!!)
-                val intent = Intent(this@login, VerifyMobile::class.java)
-                intent.putExtra("mobile", no)
-                startActivity(intent)
-                Toast.makeText(this@login, no, Toast.LENGTH_LONG).show()
-            });
         if (firebaseAuth!!.currentUser != null){
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+
 
     }
 
@@ -82,14 +97,50 @@ class login: AppCompatActivity() {
         val credential = FacebookAuthProvider.getCredential(accessToken.token)
         firebaseAuth!!.signInWithCredential(credential).addOnCompleteListener(this) {
             task ->
-            if (task.isSuccessful){
-                Log.d(TAG, "LoginCredentialSuccess")
-                val user = firebaseAuth!!.currentUser
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+            if (!task.isSuccessful) {
+                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
             }else{
-                Log.w(TAG, "signin credential failed", task.exception)
-                Toast.makeText(this, "created Account failed", Toast.LENGTH_LONG).show()
+                Log.d(TAG, "LoginCredentialSuccess")
+
+                val user = firebaseAuth!!.currentUser
+                for (profile in user!!.providerData) {
+                    // check if the provider id matches "facebook.com"
+                    if (FacebookAuthProvider.PROVIDER_ID == profile.providerId) {
+                        facebookUserId = profile.uid
+                    }
+                }
+
+
+                    ref.addListenerForSingleValueEvent(object: ValueEventListener{
+                    override fun onCancelled(p0: DatabaseError) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                         val usrName = user.displayName
+                         val userId =  user.uid
+
+
+                        val photoUrl = "https://graph.facebook.com/$facebookUserId/picture?height=500";
+                            e("TAGERR", "$photoUrl")
+                        set.addUpdateSettings(Const.PREF_MY_ID, userId!!)
+                        set.addUpdateSettings(Const.PREF_MY_NAME, usrName!!)
+                        set.addUpdateSettings(Const.PREF_MY_DP, photoUrl)
+
+                        if (!p0 .hasChild(userId!!)) {
+                            ref.child("$userId/$NODE_NAME").setValue(usrName)
+                            ref.child("$userId/$NODE_PHOTO").setValue(photoUrl)
+                            ref.child("$userId/$NODE_ID").setValue(userId)
+                        }
+                        val intent = Intent(this@login, MainActivity::class.java)
+                        startActivity(intent)
+
+
+                    }
+
+                })
+
+
             }
         }
 
